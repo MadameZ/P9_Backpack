5968f5aeb20b311ae601116bec3eac388f345f7e
Caroline Zaini <zainicaroline@gmail.com>
2020-08-05 10:26:42 +0200

    add comment in Readme

--- Backpack/Supporting FIles/ApiKey.swift
+++ Backpack/Supporting FIles/ApiKey.swift
@@ -8,11 +8,8 @@
 
 import Foundation
 
-struct ApiKey {
-    
+struct ApiKey { 
     static let fixer = "ceaad78d3a976a4bc4bb34c9c796fea4"
     static let googleTranslate = "AIzaSyD8HePYGtn7JAclDIrTJcSxmUEjXYbndzM"
     static let openWeather = "e556362e1dafdd86ff1ea56d8eacedb2"
-    
-    
 }
